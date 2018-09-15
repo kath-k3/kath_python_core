d6537e86e68c6f6150f3f807ad1c7b5a28fa1c60
Kadri_K3 <onikka@gmail.com>
2018-09-15 12:00:51 +0200

    Classes Qs and 1, 2ex

---
+++ week_03/k3_solutions/10_classes_ex1.py
@@ -0,0 +1,36 @@
+'''
+Strings methods
+Demonstrate below:
+- strip
+- replace
+- find
+Source: Exercise in chapter "Strings" in Think Python 2e:
+http://greenteapress.com/thinkpython2/html/thinkpython2009.html
+'''
+
+# strip usage
+string_1 = ' Guess who who   '
+
+# Leading whitepsace are removed
+print(string_1.strip())
+
+# prints only "uess"
+print(string_1.strip(' Gwho'))
+
+
+# replace usage
+string_2 = "this is great, this really is"
+
+# returns 'thwas was great, thwas really was'
+print(string_2.replace("is", "was"))
+
+# returns 'thwas was great, thwas really is'
+print(string_2.replace("is", "was", 3))
+
+# find usage
+string_3 = "this is string example!";
+string_4 = "exam";
+print(string_3.find(string_4))
+print(string_3.find(string_4, 10))
+
+
